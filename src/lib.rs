c4082b30c9735f9b56e60bb83d2a26c1c6dc0fc4
jerry73204 <jerry73204@gmail.com>
2020-09-24 17:04:17 +0800

    Write doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! TYP is a type-level programming langauge that computes types.
+//! It enables you to write _type operators_, the functions that translates types, in Rust syntax.
+//! Please read the [TYP book](https://github.com/jerry73204/typ-book/) understand the usage.
+
 #![feature(hash_set_entry)]
 
 mod common;
@@ -10,6 +14,7 @@ mod var;
 
 use crate::{common::*, parse::ItemVec};
 
+/// The main macro that translates the TYP langauge to actual Rust implementations.
 #[proc_macro]
 pub fn typ(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ItemVec(items) = parse_macro_input!(tokens as ItemVec);
@@ -18,11 +23,13 @@ pub fn typ(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
         .into()
 }
 
+/// Constructs a signed integer type from an integer literal.
 #[proc_macro]
 pub fn tyint(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     tyint::tyint(input)
 }
 
+/// Constructs an unsigned integer type from an integer literal.
 #[proc_macro]
 pub fn tyuint(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     tyint::tyuint(input)
